d01ae5f6c41db762995c6845998aff254de985e9
warmpain9 <warmpainq@gmail.com>
2026-07-16 01:34:25 +0300

    Update interium-loader.user.js

--- loader/interium-loader.user.js
+++ loader/interium-loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Interium Loader
 // @namespace    https://github.com/warmpain9/Interium
-// @version      2.0.0
+// @version      2.1.0
 // @description  CSP-safe loader: Tampermonkey attaches the Interium runtime from GitHub (via jsDelivr) using @require.
 // @author       Interium contributors
 // @license      MIT
@@ -15,7 +15,7 @@
 // @connect      pekora.zip
 // @connect      www.pekora.zip
 // @connect      www.koromons.net
-// @require      https://cdn.jsdelivr.net/gh/warmpain9/Interium@main/dist/interium-main-working-2.js
+// @require      https://cdn.jsdelivr.net/gh/warmpain9/Interium@main/dist/interium-main-3.js
 // @updateURL    https://cdn.jsdelivr.net/gh/warmpain9/Interium@main/loader/interium-loader.user.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/warmpain9/Interium@main/loader/interium-loader.user.js
 // @homepageURL  https://github.com/warmpain9/Interium
@@ -31,9 +31,9 @@
  * document-start - the page CSP cannot block that.
  *
  * How updates ship:
- * 1. Commit the new runtime to dist/interium-main-working-2.js on main.
+ * 1. Commit the new runtime to dist/interium-main-3.js on main.
  * 2. Bump @version in THIS file and commit it too.
  * Tampermonkey auto-updates the loader from @updateURL and re-downloads the
  * @require'd runtime along with it - users never reinstall anything.
  */
-console.info('[Interium Loader] v2.0.0 - runtime attached via @require (CSP-safe).');
+console.info('[Interium Loader] v2.1.0 - runtime attached via @require (CSP-safe).');
